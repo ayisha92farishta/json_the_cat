1681b3dd7575db5883016d5c4e8ec28b45d52243
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-08-25 00:57:17 +0000

    Wrote codes for unit testing using Mocha and Chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,20 +4,30 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
 
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,(error,response,body) => {
-  //defining the callback parameters
-    let ActiveError;
-    let desc;
-    if (error !== null) {
-      ActiveError = error;
-    } else {
-      ActiveError = false;
+    let errorMsg = null;
+    let desc = null;
+
+    //Handle request errors .
+    if (error) {
+      errorMsg = error;
+    } else if (error === null) {
       const data = JSON.parse(body);
-      desc = data[0].description;
+      if (!data.length) {
+        errorMsg = "Breed is unavailable";
+      } else {
+  
+        desc  = data[0].description;
+      }
+  
     }
+  
     //invoking the callback
-    callback(ActiveError,desc);
+
+    callback(errorMsg,desc);
 
   });
 };
 
 module.exports = { fetchBreedDescription };
+
+
